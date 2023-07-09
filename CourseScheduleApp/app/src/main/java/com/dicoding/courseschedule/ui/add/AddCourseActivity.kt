b572d0e921abbd4944d7ae02d8c54dd1a42104e4
fadelpm2002 <fadelpm2002@gmail.com>
2023-07-09 18:01:17 +0700

    course scheduler app

---
+++ CourseScheduleApp/app/src/main/java/com/dicoding/courseschedule/ui/add/AddCourseActivity.kt
@@ -0,0 +1,29 @@
+package com.dicoding.courseschedule.ui.add
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.dicoding.courseschedule.R
+
+class AddCourseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_course)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_add, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.add -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
